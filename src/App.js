e3b315c676e13785ce072dc63ff01bf6ea5ec92a
Ricardo Gallardo <rgallardouk@gmail.com>
2019-09-02 21:00:26 +0100

    Feat: add available shelves from booksAPI

--- src/App.js
+++ src/App.js
@@ -2,16 +2,27 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import BookList from './BookList'
 import BookSearch from './BookSearch'
-// import * as BooksAPI from './utils/BooksAPI'
+import * as BooksAPI from './utils/BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
-  state = {}
+  state = {
+    books: [],
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+      this.setState(() => ({
+        books,
+      }))
+    })
+  }
 
   render() {
+    const { books } = this.state
     return (
       <div className="app">
-        <Route exact path="/" component={BookList} />
+        <Route exact path="/" render={() => <BookList books={books} />} />
         <Route path="/search" component={BookSearch} />
       </div>
     )
--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,45 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import Book from './Book'
 
-function BookShelf() {
-  return (
-    <div className="bookshelf">
-      <h2 className="bookshelf-title">Currently Reading</h2>
-      <div className="bookshelf-books">
-        <ol className="books-grid">
-          <Book />
-          <li>
-            <div className="book">
-              <div className="book-top">
-                <div
-                  className="book-cover"
-                  style={{
-                    width: 128,
-                    height: 188,
-                    backgroundImage:
-                      'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")',
-                  }}
-                ></div>
-                <div className="book-shelf-changer">
-                  <select>
-                    <option value="move" disabled>
-                      Move to...
-                    </option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
-                  </select>
-                </div>
-              </div>
-              <div className="book-title">Ender&apos;s Game</div>
-              <div className="book-authors">Orson Scott Card</div>
-            </div>
-          </li>
-        </ol>
+class BookShelf extends Component {
+  static propTypes = {
+    books: PropTypes.arrayOf(PropTypes.object).isRequired,
+    shelf: PropTypes.string.isRequired,
+  }
+
+  nameSwitch = param => {
+    switch (param) {
+      case 'currentlyReading':
+        return 'Currently Reading'
+      case 'wantToRead':
+        return 'Want to Read'
+      case 'read':
+        return 'Read'
+      default:
+        return param
+    }
+  }
+
+  render() {
+    const { shelf, books } = this.props
+    const shelfBooks = books.filter(book => book.shelf === shelf)
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{this.nameSwitch(shelf)}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {shelfBooks.map(book => (
+              <Book book={book} key={book.id} />
+            ))}
+          </ol>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default BookShelf
