277efe3d4743fa01995da027e49478f8cbc88d21
Ricardo Gallardo <rgallardouk@gmail.com>
2019-09-12 13:55:13 +0100

    Feat; Search for new books to add to current shelves

--- src/BookSearch.js
+++ src/BookSearch.js
@@ -1,14 +1,41 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import BookShelf from './BookShelf'
 
 class BookSearch extends Component {
-  state = {}
+  static propTypes = {
+    searchBooks: PropTypes.arrayOf(PropTypes.object).isRequired,
+    onSearchBooks: PropTypes.func.isRequired,
+    onUpdateBookShelf: PropTypes.func.isRequired,
+    onClearSearchBooks: PropTypes.func.isRequired,
+  }
+
+  state = {
+    query: '',
+  }
+
+  timeoutId = null
+
+  updateQuery = query => {
+    const { onSearchBooks } = this.props
+    // debounce ajax search query for better performance
+    window.clearTimeout(this.timeoutId)
+    this.timeoutId = window.setTimeout(() => {
+      onSearchBooks(query)
+    }, 500)
+    this.setState(() => ({
+      query: query.trim(),
+    }))
+  }
 
   render() {
+    const { query } = this.state
+    const { searchBooks, onUpdateBookShelf, onClearSearchBooks } = this.props
     return (
       <div className="search-books">
         <div className="search-books-bar">
-          <Link to="/" className="close-search">
+          <Link to="/" className="close-search" onClick={onClearSearchBooks}>
             Close
           </Link>
           <div className="search-books-input-wrapper">
@@ -19,11 +46,22 @@ class BookSearch extends Component {
             However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
             you don't find a specific author or title. Every search is limited by search terms.
             */}
-            <input type="text" placeholder="Search by title or author" />
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={query}
+              onChange={e => this.updateQuery(e.target.value)}
+            />
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+          {searchBooks.length ? (
+            <BookShelf
+              shelf={{ shelf: undefined, name: 'Search' }}
+              onUpdateBookShelf={onUpdateBookShelf}
+              books={searchBooks}
+            />
+          ) : null}
         </div>
       </div>
     )
